f611f4b643a4c0b83d00429ba8eeeeaa1752ed08
znfnfns0365 <znfnfns0365@naver.com>
2024-06-17 15:04:27 +0900

    add handlers, modles polders

--- src/app.js
+++ src/app.js
@@ -1,20 +1,17 @@
 import express from 'express';
 import { createServer } from 'http';
 import initSocket from './init/socket.js';
-import {loadGameAssets} from './init/assets.js';
+import { loadGameAssets } from './init/assets.js';
 
 const app = express();
 const server = createServer(app);
-const PORT = 3000;
+const PORT = 3306;
 
-  //파일 읽기
-  const assets = await loadGameAssets();
-  console.log(assets);
-  
 //json 파싱
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static('public'));
+app.use(express.static('client'));
+
 initSocket(server);
 
 server.listen(PORT, async () => {
---
+++ src/handlers/register.handler.js
@@ -0,0 +1,1 @@
+// register.handler.js
